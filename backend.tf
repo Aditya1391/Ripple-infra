308629c84ab354a2137c0aa164e44f530e5a978d
Aditya1391 <145246461+Aditya1391@users.noreply.github.com>
2026-03-16 19:11:06 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "strg-rg"
-    storage_account_name = "strg0007"
-    container_name       = "tfstate"
-    key                  = "dev.tfstate"
-  }
-}
